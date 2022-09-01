df6f9caf45394d765921a7250f5fed2064ac5bda
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-09-02 00:25:51 +0530

    converted to functional component

--- src/AppFunctional.js
+++ src/AppFunctional.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
 import Header from "./components/Header";
 import UsersList from "./components/UsersList";
 
@@ -10,9 +9,40 @@ const AppFunctional = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
 
+  useEffect(() => {
+    const loadUsers = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(
+          `https://randomuser.me/api/?page=${page}&results=10`
+        );
+
+        setUsers((users) => [...users, ...response.data.results]);
+        setErrorMsg("");
+      } catch (error) {
+        setErrorMsg("Error while loading data. Try again later.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadUsers();
+  }, [page]);
+
+  const loadMore = () => {
+    setPage((page) => page + 1);
+  };
+
   return (
-    <div>
-      <h2>Functional Component</h2>
+    <div className="main-section">
+      <Header />
+      <UsersList users={users} />
+      {errorMsg && <p className="errorMsg">{errorMsg}</p>}
+      <div className="load-more">
+        <button onClick={loadMore} className="btn-grad">
+          {isLoading ? "Loading..." : "Load More"}
+        </button>
+      </div>
     </div>
   );
 };
